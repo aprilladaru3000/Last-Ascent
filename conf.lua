b8cfaa087fb9ee9d5be8e0cad88e271022920f0f
aprilladaru3000 <darupangestu3000@gmail.com>
2025-06-12 18:39:29 +0700

    Add files via upload

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+    t.tittle = "Platformer"
+    t.version = "11.5"
+    t.console = true
+    t.window.width = 1280
+    t.window.height = 720
+    t.window.vsync = 0
+end
---
+++ player.lua
@@ -0,0 +1,211 @@
+Player = {}
+
+function Player:load()
+   self.x = 100
+   self.y = 300
+   self.width = 20
+   self.height = 32
+   self.xVel = 0
+   self.yVel = 0
+   self.maxSpeed = 100
+   self.acceleration = 4000
+   self.friction = 3500
+   self.gravity = 1500
+   self.jumpAmount = -500
+
+   self.graceTime = 0
+   self.graceDuration = 0.1
+
+   self.grounded = false
+   self.hasDoubleJump = true
+
+   self.direction = "right"
+   self.state = "idle"
+
+   self:loadAssets()
+
+   self.physics = {}
+   self.physics.body = love.physics.newBody(World, self.x, self.y, "dynamic")
+   self.physics.body:setFixedRotation(true)
+   self.physics.shape = love.physics.newRectangleShape(self.width, self.height)
+   self.physics.fixture = love.physics.newFixture(self.physics.body, self.physics.shape)
+end
+
+function Player:loadAssets()
+   self.animation = {timer = 0, rate = 0.1}
+
+   self.animation.run = {total = 8, current = 1, img = {}}
+   for i=1, self.animation.run.total do
+      self.animation.run.img[i] = love.graphics.newImage("assets/graphics/player/run/"..i..".png")
+   end
+
+   self.animation.idle = {total = 7, current = 1, img = {}}
+   for i=1, self.animation.idle.total do
+      self.animation.idle.img[i] = love.graphics.newImage("assets/graphics/player/idle/"..i..".png")
+   end
+
+   self.animation.air = {total = 4, current = 1, img = {}}
+   for i=1, self.animation.air.total do
+      self.animation.air.img[i] = love.graphics.newImage("assets/graphics/player/air/"..i..".png")
+   end
+
+   self.animation.draw = self.animation.idle.img[1]
+   self.animation.width = self.animation.draw:getWidth()
+   self.animation.height = self.animation.draw:getHeight()
+end
+
+function Player:update(dt)
+   self:setState()
+   self:setDirection()
+   self:animate(dt)
+   self:decreaseGraceTime(dt)
+   self:syncPhysics()
+   self:move(dt)
+   self:applyGravity(dt)
+end
+
+function Player:setState()
+   if not self.grounded then
+      self.state = "air"
+   elseif self.xVel == 0 then
+      self.state = "idle"
+   else
+      self.state = "run"
+   end
+end
+
+function Player:setDirection()
+   if self.xVel < 0 then
+      self.direction = "left"
+   elseif self.xVel > 0 then
+      self.direction = "right"
+   end
+end
+
+function Player:animate(dt)
+   self.animation.timer = self.animation.timer + dt
+   if self.animation.timer > self.animation.rate then
+      self.animation.timer = 0
+      self:setNewFrame()
+   end
+end
+
+function Player:setNewFrame()
+   local anim = self.animation[self.state]
+   if anim.current < anim.total then
+      anim.current = anim.current + 1
+   else
+      anim.current = 1
+   end
+   self.animation.draw = anim.img[anim.current]
+end
+
+function Player:decreaseGraceTime(dt)
+   if not self.grounded then
+      self.graceTime = self.graceTime - dt
+   end
+end
+
+function Player:applyGravity(dt)
+   if not self.grounded then
+      self.yVel = self.yVel + self.gravity * dt
+   end
+end
+
+function Player:move(dt)
+   if love.keyboard.isDown("d", "right") then
+      if self.xVel < self.maxSpeed then
+         if self.xVel + self.acceleration * dt < self.maxSpeed then
+            self.xVel = self.xVel + self.acceleration * dt
+         else
+            self.xVel = self.maxSpeed
+         end
+      end
+   elseif love.keyboard.isDown("a", "left") then
+      if self.xVel > -self.maxSpeed then
+         if self.xVel - self.acceleration * dt > -self.maxSpeed then
+            self.xVel = self.xVel - self.acceleration * dt
+         else
+            self.xVel = -self.maxSpeed
+         end
+      end
+   else
+      self:applyFriction(dt)
+   end
+end
+
+function Player:applyFriction(dt)
+   if self.xVel > 0 then
+      if self.xVel - self.friction * dt > 0 then
+         self.xVel = self.xVel - self.friction * dt
+      else
+         self.xVel = 0
+      end
+   elseif self.xVel < 0 then
+      if self.xVel + self.friction * dt < 0 then
+         self.xVel = self.xVel + self.friction * dt
+      else
+         self.xVel = 0
+      end
+   end
+end
+
+function Player:syncPhysics()
+   self.x, self.y = self.physics.body:getPosition()
+   self.physics.body:setLinearVelocity(self.xVel, self.yVel)
+end
+
+function Player:beginContact(a, b, collision)
+   if self.grounded == true then return end
+   local nx, ny = collision:getNormal()
+   if a == self.physics.fixture then
+      if ny > 0 then
+         self:land(collision)
+      elseif ny < 0 then
+         self.yVel = 0
+      end
+   elseif b == self.physics.fixture then
+      if ny < 0 then
+         self:land(collision)
+      elseif ny > 0 then
+         self.yVel = 0
+      end
+   end
+end
+
+function Player:land(collision)
+   self.currentGroundCollision = collision
+   self.yVel = 0
+   self.grounded = true
+   self.hasDoubleJump = true
+   self.graceTime = self.graceDuration
+end
+
+function Player:jump(key)
+   if (key == "space" or key == "up") then
+      if self.grounded or self.graceTime > 0 then
+         self.yVel = self.jumpAmount
+         self.grounded = false
+         self.graceTime = 0
+      elseif self.hasDoubleJump then
+         self.hasDoubleJump = false
+         self.yVel = self.jumpAmount * 0.7
+      end
+   end
+end
+
+function Player:endContact(a, b, collision)
+   if a == self.physics.fixture or b == self.physics.fixture then
+      if self.currentGroundCollision == collision then
+         self.grounded = false
+      end
+   end
+end
+
+function Player:draw()
+   local scaleX = 1
+   if self.direction == "left" then
+      scaleX = -1
+   end
+   love.graphics.draw(self.animation.draw, self.x, self.y, 0, scaleX, 1, self.animation.width / 2, self.animation.height / 2)
+end
